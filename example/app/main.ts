280054c69d03745d8d803ee2c9da81e7244badea
Sebastian Fuss <sebastian.fuss@googlemail.com>
2016-04-28 17:36:03 +0200

    Update dependency to angular2-beta16

--- example/app/main.ts
+++ example/app/main.ts
@@ -1,5 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 import {AppComponent} from './app.component';
 import {provide} from 'angular2/core';
 
