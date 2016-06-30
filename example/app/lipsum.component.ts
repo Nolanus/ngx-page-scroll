ae56c5b60a89097f3ecbfd9b47d5f6ebdb981dbc
Sebastian Fuss <sebastian.fuss@googlemail.com>
2016-06-30 20:37:50 +0200

    Example for cross route linking from within the router outlet (#2)

--- example/app/lipsum.component.ts
+++ example/app/lipsum.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 import {NgFor} from '@angular/common';
 
 import {PageScroll} from 'ng2-page-scroll';
 
 @Component({
     templateUrl: './app/lipsum.component.html',
-    directives: [PageScroll, NgFor]
+    directives: [PageScroll, NgFor, ROUTER_DIRECTIVES]
 })
 export class LipsumComponent {
-    array:number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    array: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 }
