57b13de88fa75bac1b614d85dbd23c6184c2a867
Sebastian Fuss <sebastian.fuss@googlemail.com>
2016-09-01 23:43:03 +0200

    Update example to showcase new ScrollService and ScrollInstance

--- example/app/lipsum.component.ts
+++ example/app/lipsum.component.ts
@@ -2,24 +2,30 @@ import {Component, ViewChild, ElementRef} from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {NgFor} from '@angular/common';
 
-import {PageScroll, PageScrollService} from 'ng2-page-scroll';
+import {PageScroll, PageScrollService, PageScrollInstance} from 'ng2-page-scroll';
 
 @Component({
-  templateUrl: './app/lipsum.component.html',
-  directives: [PageScroll, NgFor, ROUTER_DIRECTIVES],
-  providers: [PageScrollService],
-  styles: ['#container {height:100px;overflow:auto;}']
+    templateUrl: './app/lipsum.component.html',
+    directives: [PageScroll, NgFor, ROUTER_DIRECTIVES],
+    providers: [PageScrollService],
+    styles: ['#container {height:100px;overflow:auto;}']
 })
 export class LipsumComponent {
-  array: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  constructor(private pageScrollService: PageScrollService) {
+    array: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-  }
-  @ViewChild('container') private container: ElementRef;
-  goToAnchorHead11() {
-    this.pageScrollService.scrollView('#head11');
-  };
-  goToAnchorByParentSelector(){
-    this.pageScrollService.scrollView('#inContainer', [this.container.nativeElement]);
-  }
+    constructor() {
+
+    }
+
+    @ViewChild('container') private container: ElementRef;
+
+    goToAnchorHead11() {
+        let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.container.nativeElement.ownerDocument, '#head11');
+        PageScrollService.start(pageScrollInstance);
+    };
+
+    goToAnchorByParentSelector() {
+        let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInlineInstance(this.container.nativeElement.ownerDocument, '#inContainer', this.container.nativeElement);
+        PageScrollService.start(pageScrollInstance);
+    }
 }
