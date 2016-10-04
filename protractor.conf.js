f463433ea92816a0a3ea61659b9b04604dfe5e2f
Sebastian Fuss <sebastian.fuss@googlemail.com>
2016-10-04 14:23:50 +0200

    Add missing protractor conf so one can run the e2e tests

---
+++ protractor.conf.js
@@ -0,0 +1,28 @@
+exports.config = {
+    baseUrl: 'http://localhost:4200/',
+    specs: ['test/e2e/**/*.spec.js'],
+    directConnect: true,
+    exclude: [],
+    multiCapabilities: [{
+        browserName: 'chrome'
+    }],
+    allScriptsTimeout: 110000,
+    getPageTimeout: 100000,
+    framework: 'jasmine2',
+    jasmineNodeOpts: {
+        isVerbose: false,
+        showColors: true,
+        includeStackTrace: false,
+        defaultTimeoutInterval: 400000,
+        helpers: ['node_modules/jasmine-expect/index.js']
+    },
+
+    /**
+     * ng2 related configuration
+     *
+     * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+     * `rootEl`
+     *
+     */
+    useAllAngular2AppRoots: true
+};
