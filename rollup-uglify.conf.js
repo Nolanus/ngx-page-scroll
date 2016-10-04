52b84bba3f1abe05b477e885984f1631e42c6b4a
Sebastian Fuss <sebastian.fuss@googlemail.com>
2016-10-04 14:23:05 +0200

    Add rollupjs and json file generation for aot support

---
+++ rollup-uglify.conf.js
@@ -0,0 +1,23 @@
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+    entry     : 'ng2-page-scroll.js',
+    dest      : 'bundles/ng2-page-scroll.min.js',
+    format    : 'umd',
+    external  : [
+        '@angular/core',
+        '@angular/router',
+        '@angular/platform-browser',
+        '@angular/common'
+    ],
+    globals   : {
+        '@angular/core': 'ng.core',
+        '@angular/router': 'ng.router',
+        '@angular/platform-browser': 'ng.platform-browser',
+        '@angular/common': 'ng.common'
+    },
+    moduleName: 'ng2.page.scroll',
+    plugins: [
+        uglify()
+    ]
+}
