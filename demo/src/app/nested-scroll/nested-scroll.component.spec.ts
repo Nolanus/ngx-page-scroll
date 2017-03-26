606e819b42ad868391e0620a7d2aa0dce38f41a4
Sebastian Fuss <sebastian.fuss@googlemail.com>
2017-03-26 15:01:48 +0200

    Remove spec files from demo app

--- demo/src/app/nested-scroll/nested-scroll.component.spec.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-
-import {TestBed, async} from '@angular/core/testing';
-import {NestedScrollComponent} from './nested-scroll.component';
-
-describe('Component: Nested', () => {
-    it('should ...', () => {
-    });
-});
--- demo/src/app/simple-scroll/simple-scroll.component.spec.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { SimpleScrollComponent } from './simple-scroll.component';
-
-describe('Component: SimpleScroll', () => {
-  it('should ...', () => {
-  });
-});
