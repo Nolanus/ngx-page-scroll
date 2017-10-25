f3de7c614f187d7cec9d8f34d702c565a38e7cb0
Sebastian Fuss <sebastian.fuss@googlemail.com>
2017-10-25 11:49:24 +0200

    Adjust demo and test cases to new cross-route scrolling

--- test/e2e/route-scroll.spec.ts
+++ test/e2e/route-scroll.spec.ts
@@ -12,10 +12,28 @@ describe('Route Scroll page', () => {
         return browser.driver.executeScript('return Math.round(window.pageYOffset);');
     }
 
-    // Needs fix, as angular changed the time when router events are fired and thus new page is not fully rendered yet
-    xit('should scroll open the new route and scroll to the target heading', () => {
+    it('should scroll open the new route and scroll to the target heading', () => {
         let target: ElementFinder = element(by.css('#head7'));
         let trigger: ElementFinder = element(by.css('#differentRouteScroll'));
+
+        trigger.getAttribute('routerLink').then(function (routerLink: string) {
+            trigger.sendKeys(protractor.Key.ENTER).then(() => {
+                expect(browser.getCurrentUrl()).toContain(routerLink);
+                target.getLocation().then((headingLocation: ILocation) => {
+                    browser.sleep(1250).then(() => {
+                        getScrollPos().then((pos: number) => {
+                            expect(pos).toBeCloseTo(Math.round(headingLocation.y), Closeness.ofByOne);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('should scroll open the /simple route from home and scroll to the target heading', () => {
+        browser.get('/');
+        let target: ElementFinder = element(by.css('#head3'));
+        let trigger: ElementFinder = element(by.css('#demoScroll2'));
 
         trigger.getAttribute('routerLink').then(function (routerLink: string) {
             trigger.sendKeys(protractor.Key.ENTER).then(() => {
