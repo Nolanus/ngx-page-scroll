18f9dac20b443dab03378c33227747160793d8bd
Sebastian Fuss <sebastian.fuss@googlemail.com>
2017-12-05 09:48:15 +0200

    Add workaround to work with rxjs 5.5.3 (closes #277)

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -1,17 +1,17 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
 // Components
-import {SimpleScrollComponent} from './simple-scroll/simple-scroll.component';
-import {HomeComponent} from './home/home.component';
-import {NestedScrollComponent} from './nested-scroll/nested-scroll.component';
-import {TranslatedTargetScrollComponent} from './translated-target-scroll/translated-target-scroll.component';
-import {PathologicalTestsComponent} from './pathological-tests/pathological-tests.component';
-import {HorizontalScrollComponent} from './horizontal-scroll/horizontal-scroll.component';
-import {RouterScrollComponent} from './router-scroll/router-scroll.component';
-import {NamespaceScrollComponent} from './namespace-scroll/namespace-scroll.component';
+import { SimpleScrollComponent } from './simple-scroll/simple-scroll.component';
+import { HomeComponent } from './home/home.component';
+import { NestedScrollComponent } from './nested-scroll/nested-scroll.component';
+import { TranslatedTargetScrollComponent } from './translated-target-scroll/translated-target-scroll.component';
+import { PathologicalTestsComponent } from './pathological-tests/pathological-tests.component';
+import { HorizontalScrollComponent } from './horizontal-scroll/horizontal-scroll.component';
+import { RouterScrollComponent } from './router-scroll/router-scroll.component';
+import { NamespaceScrollComponent } from './namespace-scroll/namespace-scroll.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'simple', component: SimpleScrollComponent},
   {path: 'nested', component: NestedScrollComponent},
   {path: 'translated', component: TranslatedTargetScrollComponent},
