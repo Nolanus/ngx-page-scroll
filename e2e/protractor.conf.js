d19ad914069744c806a53b50465c8e8076282ebd
Sebastian Fuss <sebastian.fuss@googlemail.com>
2019-01-09 18:00:07 +0100

    Run e2e tests in headless chrome

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -8,13 +8,20 @@ const config = {
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
+  capabilities: {
+    chromeOptions: {
+      args: ['--headless']
+    },
+    'browserName': 'chrome'
+  },
   directConnect: false,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {
+    }
   },
   onPrepare() {
     require('ts-node').register({
@@ -23,44 +30,5 @@ const config = {
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
-
-if (process.env.TRAVIS) {
-
-  var capabilities = [
-    // [platform, browsername, version]
-    //['macOS 10.14', 'chrome', '71.0'],
-    ['macOS 10.14', 'chrome', '70.0'],
-    //['macOS 10.14', 'chrome', '68.0'],
-    //['Windows 10', 'MicrosoftEdge', '16.16299'],
-    //['Windows 10', 'MicrosoftEdge', '15.15063'],
-    // TODO Check why getting window height does not work in android any more
-    // ['Linux', 'android', '6.0']
-    // TODO Selenium Driver problem for the following
-    // ['macOS 10.12', 'safari', '10.0'],
-    // ['Windows 10', 'internet explorer', '11.103'],
-    // ['Windows 10', 'firefox', '50.0'],
-    // TODO Problem calculating the target position in tests
-    // ['OS X 10.10', 'iphone', '10.0'],
-    // ['OS X 10.10', 'iphone', '9.3']
-  ];
-
-  // Override the baseUrl, as the port is a different one
-  config.baseUrl = 'http://localhost:8000/';
-  config.multiCapabilities = capabilities.map(function (capability) {
-    return {
-      browserName: capability[1],
-      platform: capability[0],
-      version: capability[2],
-      shardTestFiles: true,
-      name: 'NgxPageScroll',
-      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-    }
-  });
-  config.sauceUser = process.env.SAUCE_USERNAME;
-  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.sauceBuild = 'travis-build#' + process.env.TRAVIS_BUILD_NUMBER;
-} else {
-  config.multiCapabilities = [{browserName: 'chrome'}];
-}
 
 exports.config = config;
