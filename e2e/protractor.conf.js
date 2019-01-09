38eeffce6e1b8add1e6092080a7fa32ce3a88ff2
Sebastian Fuss <sebastian.fuss@googlemail.com>
2019-01-09 16:34:08 +0100

    Try with single saucelabs browser for the beginning

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -8,7 +8,7 @@ const config = {
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  directConnect: false,
+  directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
@@ -28,11 +28,11 @@ if (process.env.TRAVIS) {
 
   var capabilities = [
     // [platform, browsername, version]
-    ['macOS 10.14', 'chrome', '71.0'],
+    //['macOS 10.14', 'chrome', '71.0'],
     ['macOS 10.14', 'chrome', '70.0'],
-    ['macOS 10.14', 'chrome', '68.0'],
-    ['Windows 10', 'MicrosoftEdge', '16.16299'],
-    ['Windows 10', 'MicrosoftEdge', '15.15063'],
+    //['macOS 10.14', 'chrome', '68.0'],
+    //['Windows 10', 'MicrosoftEdge', '16.16299'],
+    //['Windows 10', 'MicrosoftEdge', '15.15063'],
     // TODO Check why getting window height does not work in android any more
     // ['Linux', 'android', '6.0']
     // TODO Selenium Driver problem for the following
